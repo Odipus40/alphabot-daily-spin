e1eaf23f7f28d1055d2b6fad9dfa651760bfcdb8
Odipus <109467219+Odipus40@users.noreply.github.com>
2025-02-12 20:24:38 +0700

    Update daily.js

--- daily.js
+++ daily.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const axios = require('axios');
 const moment = require('moment-timezone');
 const readline = require('readline');
+require('colors');
 const { displayHeader } = require('./helpers');
 
 const LOGIN_API = 'https://www.alphabot.app/api/auth/session';
