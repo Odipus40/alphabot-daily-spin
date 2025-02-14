c3390a920c3064eea103ff6322d80ae60945527b
Odipus <109467219+Odipus40@users.noreply.github.com>
2025-02-14 09:27:37 +0700

    Update daily.js

--- daily.js
+++ daily.js
@@ -3,16 +3,23 @@ const { ethers } = require('ethers');
 const axios = require('axios');
 
 const API_URL = 'https://www.alphabot.app/api/platformAirdrops/';
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const WALLET = new ethers.Wallet(PRIVATE_KEY);
 
 async function login() {
     try {
+        if (!window.ethereum) {
+            throw new Error('MetaMask is not installed');
+        }
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send('eth_requestAccounts', []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
         const message = "Login to Alphabot";
-        const signature = await WALLET.signMessage(message);
+        const signature = await signer.signMessage(message);
 
         const response = await axios.post(API_URL, {
-            address: WALLET.address,
+            address: address,
             signature: signature
         });
 
